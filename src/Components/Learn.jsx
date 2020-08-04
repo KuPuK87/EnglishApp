bed3cd134c82ec6059e1510470fc050abda3a970
Kirill Bogdanov <kupuk.87@gmail.com>
2020-08-04 22:42:23 +0300

    Update

--- src/Components/Learn.jsx
+++ src/Components/Learn.jsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Learn = () => {
+    const library = JSON.parse(localStorage.getItem('library')) || [{ id: 0, word: '', translate: '' }]
+    const [index, setIndex] = useState(0)
+    const [end, setEnd] = useState(false)
+    const word = library[index]
+    const repeatLearn = () => {
+        setEnd(false)
+        setIndex(0)
+    }
+    const nextWord = () => {
+        if (library.length - 1 !== index) {
+            setIndex(index + 1)
+        } else {
+            setEnd(true)
+        }
+
+    }
     return (
-        <div>
-            Learn
+        <div className='learn_wrapper'>
+            <div className='learn_container'>
+                {!end ? <div className='percentage'>50%</div> :
+                    null}
+                <div className='word_translation'>
+                    {!end ? word.translate : <div>
+                        Well done!
+                        <div onClick={repeatLearn} className='btn_repeat'>
+                            &#10227;
+                        </div>
+                    </div>}
+                </div>
+                {!end ? <div className='word_container'>
+                    <span className='description_label'>Translation</span>
+                    <span className='word'>{word.word}</span>
+                </div> : null}
+            </div>
+            {!end ?
+                <div onClick={nextWord} className='btn_next'>
+                    &#8594;
+            </div>
+                :
+                null
+            }
         </div>
     )
-
 }
 
 export default Learn;
