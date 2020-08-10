77528c993ad88dc61eca6b2264529ffefc63c97c
Kirill Bogdanov <kupuk.87@gmail.com>
2020-08-10 22:29:25 +0300

    update

--- src/Components/Games/CheckMode.jsx
+++ src/Components/Games/CheckMode.jsx
@@ -2,21 +2,35 @@ import React, { useEffect, useState } from 'react';
 
 export default (props) => {
 
-    const checkWord = () => {
-        props.setScore(props.score + 1)
+    const checkWord = (word) => {
+        if (library.length - 1 !== currentWordIndex) {
+            if (word === library[currentWordIndex].word) {
+                props.setCorrectAnswer(props.correctAnswer + 1)
+                props.setScore(props.score + 1)
+                setCurrentWordIndex(currentWordIndex + 1)
+            } else {
+                props.setWrongAnswer(props.wrongAnswer + 1)
+            }
+        } else {
+            alert('Game Over')
+            props.setCorrectAnswer(0)
+            props.setWrongAnswer(0)
+            setCurrentWordIndex(0)
+        }
     }
 
     const [currentWordIndex, setCurrentWordIndex] = useState(0)
     const [library, setLibrary] = useState(JSON.parse(localStorage.getItem('library')))
+    const [checkArr, setCheckArr] = useState([])
     const currentWord = library[currentWordIndex].translate
     const [initialScore, setInitialScore] = useState(props.score)
 
     useEffect(() => {
         const filterArr = library.filter((item, index) => index !== currentWordIndex)
         filterArr.sort(() => Math.random() - 0.5)
-        const checkArr = [filterArr[0].word, filterArr[1].word, library[currentWordIndex].word] 
-        checkArr.sort(() => Math.random() - 0.5)
-    }, [])
+        const checkArr = [filterArr[0].word, filterArr[1].word, library[currentWordIndex].word]
+        setCheckArr(checkArr.sort(() => Math.random() - 0.5))
+    }, [props.correctAnswer])
 
     useEffect(() => {
         return () => {
@@ -24,26 +38,19 @@ export default (props) => {
         }
         //componentWillUnmount
     }, [])
-    console.log(initialScore)
-    console.log(props.score)
 
     return (
         <div className='mode_wraper'>
             <div className='mode_title-word'>
-                Forest
-          </div>
+                {currentWord}
+            </div>
             <p className='mode_title-word-description'>Set translation fo this word</p>
 
             <div className='check_item-block'>
-                <div className='check_item' onClick={checkWord}>
-                    Undefined
-                </div>
-                <div className='check_item'>
-                    Undefined
-                </div>
-                <div className='check_item'>
-                    Undefined
-                </div>
+                {checkArr.map((item, index) => <div key={index} className='check_item' onClick={() => checkWord(item)}>
+                    {item}
+                </div>)}
+
             </div>
         </div>
     )
--- src/Components/Games/Game.jsx
+++ src/Components/Games/Game.jsx
@@ -1,23 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import CheckMode from "./CheckMode";
 import WriteMode from "./WriteMode";
 
 export default (props) => {
     const location = useLocation()
+    const [correctAnswer, setCorrectAnswer] = useState(0)
+    const [wrongAnswer, setWrongAnswer] = useState(0)
     return (
         <div className='game_page'>
             <NavLink to='/training'>
                 <button className='btn_exit'>Exit</button>
             </NavLink>
             <div className='points_block'>
-                <span className='correct_title'>Correct:</span>
-                <span className='error_title'>Error:</span>
+                <span className='correct_title'>Correct: {correctAnswer}</span>
+                <span className='error_title'>Error: {wrongAnswer}</span>
             </div>
             {location.pathname === '/training/check-mode' ?
                 <CheckMode
                     setScore={props.setScore}
                     score={props.score}
+                    correctAnswer={correctAnswer}
+                    wrongAnswer={wrongAnswer}
+                    setCorrectAnswer={setCorrectAnswer}
+                    setWrongAnswer={setWrongAnswer}
                 /> :
                 location.pathname === '/training/write-mode' ?
                     <WriteMode
